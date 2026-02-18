964241dd641bbeb5093feff081113404ea551ea9
Abhishek Deshmukh <deshmukhabhi584@gmail.com>
2026-02-18 15:58:58 +0530

    Add files via upload

---
+++ Student1.java
@@ -0,0 +1,30 @@
+package program.core;
+
+public class Student1 {
+	
+
+		public static void main(String[] args) {
+
+			byte Std_age = 23;
+			short Std_id = 24563;
+			int Std_eligbit_no = 709156321;
+			long Std_mobile_no = 8928795896l;
+			float Std_markes = 99.99f;
+			double Std_hight = 5.72;
+			boolean Cheek_pass = true;
+			char Std_char = '$';
+
+			System.out.println("student age =" + Std_age);
+			System.out.println("student id no =" + Std_id);
+			System.out.println("student eligbity no=" + Std_eligbit_no);
+			System.out.println("student Mobile no =" + Std_mobile_no);
+			System.out.println("student Marks =" + Std_markes);
+			System.out.println("student hight =" + Std_hight);
+			System.out.println("student is passing cheek =" + Cheek_pass);
+			System.out.println("student chara  =" + Std_char);
+
+		
+
+	}
+
+}
