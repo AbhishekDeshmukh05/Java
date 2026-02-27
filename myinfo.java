0fdfa1b0acfd465c651f60c811fa70232050824e
Abhishek Deshmukh <deshmukhabhi584@gmail.com>
2026-02-27 10:11:17 +0530

    Add files via upload

---
+++ myinfo.java
@@ -0,0 +1,21 @@
+public class myinfo {
+	public static void main(String[] args) {
+		System.out.println( " Name - Abhishek Deshmukh");
+		System.out.println(" Age - 21");
+		System.out.println(" City - Pune");
+		System.out.println(" State - Maharashtra ");
+		System.out.println(" Mobile No. - 8928791825");
+		System.out.println(" Nationality - Indian ");
+		System.out.println(" Blood group - B+ " );
+		System.out.println(" Aadhar No. - 552542512541");
+		System.out.println(" Pan No. - AVMP5241F");
+		System.out.println(" Job role - Software Tester");
+		System.out.println(" Date of birth - 01/01/2001");
+		System.out.println(" Mail id - abhi@gmail.com");
+		System.out.println(" Batch code - 1320");
+		System.out.println(" Education - BCA");
+		System.out.println(" Alternate mobile No. - 84579632100 ");
+		System.out.println(" Religion - Hindu ");
+	}
+
+}
