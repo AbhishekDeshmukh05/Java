b94280e793e5e50494a3748829d59d79dccf4063
Abhishek Deshmukh <deshmukhabhi584@gmail.com>
2026-03-04 17:25:54 +0530

    Update Assignment3.java

--- Operators/Assignment3.java
+++ Operators/Assignment3.java
@@ -6,17 +6,17 @@ public class Assignment3 {
 		int i = 2;
         i++;                // 3
         --i;                // 2
-        System.out.println(i++);    // Prints 2 then 3)
+        System.out.println(i++);    // Prints 2 then 3
         i++;                // 4
         System.out.println(i % 6);  // 4 % 6 = 4
         --i;                // 3
         System.out.println(i += 2); // 3 + 2 = 5
         i++;                // 6
         int b = i + 1;      // 7
-        System.out.println(i++);    // Prints 6 then 7)
+        System.out.println(i++);    // Prints 6 then 7
         --i;                // 6
         i %= 5;             // 6 % 5 = 1
-        System.out.println(i++);    // Prints 1 then 2)
+        System.out.println(i++);    // Prints 1 then 2
         ++i;                // 3
         System.out.println(i + 2);  // 3 + 2 = 5
         System.out.println("______________________________");
@@ -26,3 +26,4 @@ public class Assignment3 {
 	}
 
 }
+
