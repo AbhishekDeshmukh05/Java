b8c9d1fc5c51ea640f686f8d5858d3fbce9e78d6
Abhishek Deshmukh <deshmukhabhi584@gmail.com>
2026-03-30 21:06:49 +0530

    Add files via upload

---
+++ Constructor/My_Portfolio.java
@@ -0,0 +1,18 @@
+package constructor;
+
+public class My_Portfolio {
+	
+	public void My_port_Folio(String fname,String lname,int age,long mob_No,String email) {
+		System.out.println("my port folio");
+		
+		System.out.println("first :- "+fname);
+		System.out.println("last name:- "+lname);
+		System.out.println("age:- "+age);
+		System.out.println("mobile no:- "+mob_No);
+		System.out.println("email id:- "+email);
+		
+		
+		
+ }
+
+}
