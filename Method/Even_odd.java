876c70b32e88b829d63bd9db994a8b2f2e4a8b32
Abhishek Deshmukh <deshmukhabhi584@gmail.com>
2026-03-30 21:18:14 +0530

    Add files via upload

---
+++ Method/Even_odd.java
@@ -0,0 +1,41 @@
+package methods;
+
+public class Even_odd {
+	
+	int num;
+//	
+//	public void even_odd_No() {
+//		if (num%2==0) {
+//			System.out.println(num+" even no");
+//		}else {
+//			System.out.println(num+" odd no");
+//		}
+//	}
+
+//	public String even_odd_No() {
+//		if (num%2==0) {
+//			return num+" even no";
+//		}else {
+//			return num+" odd no";
+//		}
+
+//		public void even_odd_No(int num) {
+//			if (num%2==0) {
+//				System.out.println(num+" even no");
+//			}else {
+//				System.out.println(num+" odd no");
+//			}
+
+//   }
+
+//	public String even_odd_No(int num) {
+//		if (num % 2 == 0) {
+//			return num+" even no";
+//		} else {
+//			return num+" odd no";
+//		}
+//
+//	}
+	
+
+}
