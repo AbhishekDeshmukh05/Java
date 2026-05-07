edf1d98f8fafb9bf0240a7d93f33e9e00fc2c462
Abhishek Deshmukh <deshmukhabhi584@gmail.com>
2026-05-07 15:10:21 +0530

    Add files via upload

---
+++ Serialization/Emp_Info.java
@@ -0,0 +1,19 @@
+package serialization;
+
+import java.io.Serializable;
+
+public class Emp_Info implements Serializable {
+	int id;
+	String name;
+	String location;
+	public Emp_Info(int id,String name,String location) {
+		
+		this.id=id;
+		this.name=name;
+		this.location=location;
+		
+	}
+
+}
+
+
